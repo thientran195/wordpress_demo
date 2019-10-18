d8c00a416938b5ccc06579fbc9b890dcd77a5bc6
Thien Tran <tranngocthien195@gmail.com>
2019-10-18 18:08:06 +0700

    Init nginx

--- rmc.sh
+++ rmc.sh
@@ -1,2 +1,15 @@
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
+APP_NAME=$1
+if [ $APP_NAME ]
+then
+  CONTAINER=`docker ps -aq -f name=$APP_NAME`
+  if [ "$CONTAINER"!='' ]
+  then
+    docker stop $(docker ps -aq -f name=$APP_NAME)
+    docker rm $(docker ps -aq -f name=$APP_NAME)
+  else
+    echo "$APP_NAME doesn't exist"
+  fi
+else
+  echo "please pass app name argument"
+fi
+
